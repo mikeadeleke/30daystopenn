3b6390e6929f7901d9448caf546b9a284b203850
Mike Adeleke <madeleke13@gmail.com>
2014-02-17 23:08:16 -0500

    fixing js to show clicks properly

--- application.js
+++ application.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-	$(".click").click(function() {
-		$(".box2 h6").toggleClass("hidden");
+	$(".hidden").hide();
+	$(".click").click(function(event) {
+		$(event.target).parent()[0].children[3].html("display", "show");
 	});
 })
+
+
+
+
