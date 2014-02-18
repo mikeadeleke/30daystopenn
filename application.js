840beca16cfd73021398bac1b5517bbadd1d44bd
Mike Adeleke <madeleke13@gmail.com>
2014-02-17 23:59:11 -0500

    changed to toggle class

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$(".hidden").hide();
 	$(".click").click(function(event) {
-		$(event.target).parent()[0].children[3].html("display", "show");
+		$(".click").next().toggleClass("hidden");
 	});
 })
 
