6d48f44f4cca69a6840bb7c4689fbf5537a3fb45
Mike Adeleke <madeleke13@gmail.com>
2014-03-05 18:11:06 -0600

    changes

--- influence.js
+++ influence.js
@@ -5,24 +5,13 @@ $(document).ready(function() {
         success: function (data) {
             $.ajax({
                 dataType: 'jsonp',
-                url: "http://api.klout.com/v2/user.json/" + data.id + "/influence?key=3xqvjzmjz8876tetq2rahm5v",
-                success: function (subData) {
-                    $('#influence').append(subData.influence);
+                url: "http://api.klout.com/v2/79938903459509669/3xqvjzmjz8876tetq2rahm5v/influence",
+                success: function (data) {
+                    $('#influence').html('<a href="">' + data.myInfluencer + '</a>');
                 }
             });
+            console.log(data);
+            console.log(data.score);
         }
     });
 });
-// $.ajax({
-//         dataType: 'jsonp',
-//         url: 'http://api.klout.com/v2/identity.json/twitter?screenName=MikeAdeleke&key=3xqvjzmjz8876tetq2rahm5v',
-//         success: function (data) {
-//             $.ajax({
-//                 dataType: 'jsonp',
-//                 url: 'http://api.klout.com/v2/user.json/' + data.id + '/score?key=3xqvjzmjz8876tetq2rahm5v',
-//                 success: function (subData) {
-//                     $('#score').append(subData.score);
-//                 }
-//             });
-//         }
-//     });
