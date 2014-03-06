fc5448229dab28fbdc8e8ab836455a59b5e14fe8
Mike Adeleke <madeleke13@gmail.com>
2014-03-06 11:30:55 -0600

    udpates

--- influence.js
+++ influence.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
         success: function (data) {
             $.ajax({
                 dataType: 'jsonp',
-                url: "http://api.klout.com/v2/79938903459509669/3xqvjzmjz8876tetq2rahm5v/influence",
-                success: function (data) {
-                    $('#influence').html('<a href="">' + data.myInfluencer + '</a>');
+                url: 'http://api.klout.com/v2/user.json/' + data.id + '/influence?key=3xqvjzmjz8876tetq2rahm5v',
+                success: function (subData) {
+                    $('#influence').append(subData.influence);
+                    console.log(subData.myInfluencees[0]);
+                    console.log(subData.influence);
                 }
             });
-            console.log(data);
-            console.log(data.score);
         }
     });
 });
